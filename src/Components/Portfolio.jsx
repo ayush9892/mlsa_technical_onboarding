1c7b7e75e2467580ce258a4c531105e784bdcd05
Ayush Kumar <85745368+ayush9892@users.noreply.github.com>
2023-12-28 14:45:06 +0000

    Project List Updated

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -5,28 +5,16 @@ const imageAltText = "a computer keyboard with blue backlighting";
 
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Azure Databricks Formula1 Project",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Data Engineering ETL Project on Formula1 Racing using Azure Databricks and Azure Data Factory .",
+    url: "https://github.com/ayush9892/Azure_Databricks_Formula1",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Azure Data Factory COVID_19 Reporting Project",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Data Engineering ETL Project on Covid19 Reporting Using Azure Data Factory and Databricks.",
+    url: "https://github.com/ayush9892/Azure_Data_Factory_COVID_19_Reporting",
   },
 ];
 
